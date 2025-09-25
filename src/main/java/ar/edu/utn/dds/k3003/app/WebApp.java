6ecf7cc86cbc68bc394535411e5b401f2b81d719
Agus-Righetti <arighetti@frba.utn.edu.ar>
2025-09-25 12:18:28 -0300

    Se agrega WebApp

---
+++ src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
@@ -0,0 +1,25 @@
+package ar.edu.utn.dds.k3003.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "ar.edu.utn.dds.k3003.controller",
+        "ar.edu.utn.dds.k3003.config",
+        "ar.edu.utn.dds.k3003.model",
+        "ar.edu.utn.dds.k3003.repository",
+        "ar.edu.utn.dds.k3003.app"
+})
+@EnableJpaRepositories(basePackages = "ar.edu.utn.dds.k3003.repository")
+@EntityScan(basePackages = "ar.edu.utn.dds.k3003.model")
+public class WebApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WebApp.class, args);
+    }
+}
